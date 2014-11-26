6013f16f0a9df79e8e1f1a23aaad0c48e46ad2b2
wnagyufeisb <827924112@qq.com>
2014-11-26 16:53:57 +0800

    haha

--- yufei1_db.sql
+++ yufei1_db.sql
@@ -1,7 +1,7 @@
 /*==============================================================*/
 /* Database name:  yufei1_db                                    */
 /* DBMS name:      Microsoft SQL Server 2005                    */
-/* Created on:     2014/11/26 15:05:26                          */
+/* Created on:     2014/11/26 16:51:18                          */
 /*==============================================================*/
 
 
@@ -24,9 +24,9 @@ go
 /* Table: user_info                                             */
 /*==============================================================*/
 create table user_info (
-   user_id              varchar(100)         not null,
+   user_id              int                  not null,
    user_sex             varchar(10)          not null,
-   user_phone           int                  null,
+   user_phone           varchar(60)          null,
    user_state           int                  not null,
    constraint PK_USER_INFO primary key (user_id)
 )
@@ -36,10 +36,10 @@ go
 /* Table: user_key                                              */
 /*==============================================================*/
 create table user_key (
-   user_id              varchar(50)          not null,
+   user_id              int                  identity(1000,1),
    user_name            varchar(100)         not null,
    password             varchar(100)         not null,
-   pstring              varchar(20)          null,
+   pstring              varchar(20)          not null,
    constraint PK_USER_KEY primary key (user_id)
 )
 go
@@ -50,7 +50,7 @@ go
 create table user_log (
    code                 int                  identity(1000 ,1),
    user_name            varchar(100)         not null,
-   use_load_datetime    datetime             not null,
+   use_load_datetime    datetime             not null default getdate(),
    user_ip              varchar(100)         not null,
    constraint PK_USER_LOG primary key (code)
 )
